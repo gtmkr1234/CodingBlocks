d50eca0dc2e3cfee269f0c409a2270d50aaae202
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-16 18:20:24 +0530

    commit

--- src/class1_2.java
+++ src/class1_2.java
@@ -1,9 +1,9 @@
-public class class12 {
+public class class1_2 {
     public static void main(String[] args) {
         int n = 10;
         for(int i=n; i>0; i--){
             for(int j=1; j<= n-i; j++){
-                System.out.print("  ");
+                System.out.print("    ");
             }
             for(int j=0; j<i; j++){
                 System.out.print("* ");
