298113dfce5296d126da6c4271ed441449f21ad6
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-16 17:58:36 +0530

    commit

---
+++ src/Class1.java
@@ -0,0 +1,37 @@
+public class Class1 {
+    public static void main(String[] args) {
+        for(int i=3; i>=0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.print(" ");
+            for(int j=0; j<3-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=0; j<3-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i=0; i<=3; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.print(" ");
+            for(int j=0; j<3-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=0; j<3-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
