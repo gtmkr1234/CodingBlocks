b6c11094286d90f5600591974c0baf875f73f533
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-19 23:20:09 +0530

    commit added basic star patterns

--- src/class2_1.java
+++ src/class2_1.java
@@ -1,20 +1,49 @@
 public class class2_1 {
     public static void main(String[] args) {
         int n=5;
-        int nst = 2*n-1;
-        int nsp = 0;
+        int nst = 1;
+        int nsp = n-1;
 
         int row =0;
-        while(row++<n){
-            for (int i = 0; i < nsp; i++) {
-                System.out.print("  ");
-            }
-            nsp++;
-            for (int i = 0; i < nst; i++) {
-                System.out.print("* ");
-            }
-            nst-=2;
-            System.out.println();
-        }
+//        while(row++<n){
+//            for (int i = 0; i < nsp; i++) {
+//                System.out.print("  ");
+//            }
+//            nsp++;
+//            for (int i = 0; i < nst; i++) {
+//                System.out.print("* ");
+//            }
+//            nst-=2;
+//            System.out.println();
+//        }
+        /*
+         * * * * * * *
+         * * * * *
+         * * *
+         *
+         */
+
+
+
+int trow = (2*n)-1;
+while(row<trow){
+    for (int i = 0; i < nsp; i++) {
+        System.out.print("  ");
+    }
+    for (int i = 0; i < nst; i++) {
+        System.out.print("* ");
+    }
+    if(row< trow/2){
+        nsp--;
+        nst++;
+    }
+    else{
+        nst--;
+        nsp++;
+    }
+    System.out.println();
+    row++;
+}
+
     }
 }
