b6bff7d47c33662b0514c7ea0490f8b154c856c4
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-21 11:33:01 +0530

    commit added basic star patterns

--- src/class3_1.java
+++ src/class3_1.java
@@ -2,25 +2,32 @@ public class class3_1 {
     public static void main(String[] args) {
         int n=7;
         int row=0;
-        int nst=6;
-        int nsp = 1;
-        while(row<n){
-            for (int i = 0; i < nst/2; i++) {
+        int nst1=n/2 + 1;
+        int nst2 = n/2 ;
+        int nsp = -1;
+        while(row<n+1){
+            for (int i = 0; i < nst1 ; i++) {
                 System.out.print("* ");
             }
-            for (int i = 0; i < nsp; i++) {
+            for (int i = 0; i < 2*nsp; i++) {
                 System.out.print("  ");
             }
-            for (int i = 0; i < nst/2; i++) {
+            if(row==n-1){
+                nst2--;
+            }
+            for (int i = 0; i < nst2;  i++) {
                 System.out.print("* ");
             }
             if(row<n/2){
-                nst-=2;
-                nsp+=2;
+                nst1--;
+                if(row !=0 ){
+                nst2--;}
+                nsp+=1;
             }
             else{
-                nst+=2;
-                nsp-=2;
+                nst1+=1;
+                nst2++;
+                nsp-=1;
             }
             System.out.println();
             row++;
