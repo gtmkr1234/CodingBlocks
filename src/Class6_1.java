eca3c38db8d801b396e236acfb5283c534ef1663
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-26 10:31:12 +0530

    commit added basic star patterns

---
+++ src/Class6_1.java
@@ -0,0 +1,5 @@
+public class Class6_1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/class5_1.java
@@ -0,0 +1,12 @@
+public class class5_1 {
+    static void inverse(int n, int num){
+        int res=0;
+        while(n>1){
+            res = 
+            n--;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
