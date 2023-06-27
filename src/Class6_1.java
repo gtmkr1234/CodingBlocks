34087b39b82c529a30ea2cd37f04e94be168d8cd
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-27 16:37:35 +0530

    commit added basic star patterns

--- src/Class6_1.java
+++ src/Class6_1.java
@@ -1,5 +1,14 @@
 public class Class6_1 {
+    static int funct(int[] arr, int key){
+        for (int i = arr.length-1; i >0 ; i--) {
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-
+        int []arr = {1,2,3,-10,-11,2,7,9,2,11};
+        System.out.println(funct(arr, 2));
     }
 }
