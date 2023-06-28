cf941d116ebf3d0c6d969eb4a8bacc307db2a96d
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-28 21:11:04 +0530

    commit added basic star patterns

--- src/class8_2.java
+++ src/class8_2.java
@@ -1,20 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.*;
+
 public class class8_2 {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4};
         int max =0;
+        List<Integer> res = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
                 int sum=0;
+                List<Integer> temp= new ArrayList<>();
                 for (int k = i; k <= j; k++) {
                     sum+=arr[k];
-                    System.out.print(arr[k]+" ");
+                    temp.add(arr[k]);
                 }
-                max = Math.max(sum,max);
-                System.out.println();
+                if(sum>max){
+                    res = temp;
+                    max = Math.max(sum,max);
+                }
             }
-            System.out.println();
         }
 
         System.out.println("Maximum sum of subarray is: " + max);
+        System.out.println("Maximum sub array is : "+ res);
     }
 }
