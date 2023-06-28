6cd8026ea12c4ba7040fce648fbfc859d1a631cd
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-28 21:01:19 +0530

    commit added basic star patterns

--- src/class8_2.java
+++ src/class8_2.java
@@ -1,15 +1,20 @@
 public class class8_2 {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4};
+        int max =0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
+                int sum=0;
                 for (int k = i; k <= j; k++) {
+                    sum+=arr[k];
                     System.out.print(arr[k]+" ");
                 }
+                max = Math.max(sum,max);
                 System.out.println();
-
             }
             System.out.println();
         }
+
+        System.out.println("Maximum sum of subarray is: " + max);
     }
 }
